1b9850124bbe29878883371b8851073de9746c87
marcelo-rafael <marcelo.rafael.goncalves@hotmail.com>
2020-06-29 17:54:39 -0300

    primeiro componente #01

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import "./index.css";
 import ReactDOM from "react-dom";
 import React from "react";
 
-const tag = <strong>Olá React!</strong>;
+import Primeiro from "./components/basicos/Primeiro";
 
-ReactDOM.render(<div>{tag}</div>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <Primeiro></Primeiro>
+  </div>,
+  document.getElementById("root")
+);
