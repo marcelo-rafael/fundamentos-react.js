2a87842bd31470b0fc2507da788912fe1963c8b9
marcelo-rafael <marcelo.rafael.goncalves@hotmail.com>
2020-06-29 20:40:05 -0300

    desafio número aleatório

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
+import Aleatorio from './components/basicos/Aleatorio'
 
 export default () => (
   <div id="app">
     <h1>Fundamentos React</h1>
+    <Aleatorio min={1} max={60} />
     <Fragmento />
     <ComParametro titulo="Situação do Aluno" aluno="Marcelo" nota={10.0} />
     <Primeiro></Primeiro>
