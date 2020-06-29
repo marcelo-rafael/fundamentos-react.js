32fbc90d2b8f2283addeb9be06650ea2801097a9
marcelo-rafael <marcelo.rafael.goncalves@hotmail.com>
2020-06-29 19:14:05 -0300

    react fragment

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import React from "react";
 
 import Primeiro from "./components/basicos/Primeiro";
 import ComParamentro from "./components/basicos/ComParametro";
+import Fragmento from "./components/basicos/Fragmento";
 
 ReactDOM.render(
   <div id="app">
     <Primeiro></Primeiro>
-    <ComParamentro
-      titulo="Situação do Aluno"
-      aluno="Marcelo"
-      nota={10.0}
-    ></ComParamentro>
+    <ComParamentro titulo="Situação do Aluno" aluno="Marcelo" nota={10.0} />
+    <Fragmento />
   </div>,
   document.getElementById("root")
 );
