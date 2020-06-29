45bf408d05dd815dff3148224b9bf88a5acff15a
marcelo-rafael <marcelo.rafael.goncalves@hotmail.com>
2020-06-29 20:24:54 -0300

    componente App

--- src/index.js
+++ src/index.js
@@ -2,15 +2,6 @@ import "./index.css";
 import ReactDOM from "react-dom";
 import React from "react";
 
-import Primeiro from "./components/basicos/Primeiro";
-import ComParamentro from "./components/basicos/ComParametro";
-import Fragmento from "./components/basicos/Fragmento";
+import App from "./App";
 
-ReactDOM.render(
-  <div id="app">
-    <Primeiro></Primeiro>
-    <ComParamentro titulo="Situação do Aluno" aluno="Marcelo" nota={10.0} />
-    <Fragmento />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
