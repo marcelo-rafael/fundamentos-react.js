eb6ac8a307e7c41706d7829d70791b90f1227d01
marcelo-rafael <marcelo.rafael.goncalves@hotmail.com>
2020-06-29 19:45:45 -0300

    componente App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Primeiro from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametro";
+import Fragmento from "./components/basicos/Fragmento";
+
+export default () => (
+  <div id="app">
+    <h1>Fundamentos React</h1>
+    <Fragmento />
+    <ComParametro titulo="Situação do Aluno" aluno="Marcelo" nota={10.0} />
+    <Primeiro></Primeiro>
+  </div>
+);
